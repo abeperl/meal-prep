b26cf41c8bed49e8516706d4c623a8459305e127
Abe Perl <abeperl@gmail.com>
2018-12-21 12:33:36 -0500

    init commit

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: 'AIzaSyBiDYUjpwm2ht_XdfXKcdSdK-g9Niplln8',
+    authDomain: 'meal-prep-8b560.firebaseapp.com',
+    databaseURL: 'https://meal-prep-8b560.firebaseio.com',
+    projectId: 'meal-prep-8b560',
+    storageBucket: 'meal-prep-8b560.appspot.com',
+    messagingSenderId: '808336362486'
+};
+firebase.initializeApp(config);
